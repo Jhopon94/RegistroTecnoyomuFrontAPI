838667e676dadafaea7c7d3cad73c6c92b8b7ba8
John Hader Medina Pacheco <jhopon94@gmail.com>
2024-05-13 02:35:08 -0500

    Terminada toda la Gestión de los Equipos

--- src/componentes/ModalListaEqEntregados.js
+++ src/componentes/ModalListaEqEntregados.js
@@ -1,11 +1,57 @@
 import './css/modalListaEqEntregados.css';
 import BotonForm from './BotonForm';
 import VentanaDetallesEqEntregado from './VentanaDetallesEqEntregado';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { BuscarEquiposEntregados } from '../apis/equipoApi';
+import ListaFiltrada from './FiltroBusqueda';
 
 function ModalListaEqEntregados({ isOpen, onClose }) {
 
-    const [ abrirDetallesEqEntregado, setAbrirDetallesEqEntregado ] = useState(false);
+    const [abrirDetallesEqEntregado, setAbrirDetallesEqEntregado] = useState(false);
+    const [listaEqEntregadosCompleta, setListaEqEntregadosCompleta] = useState([]);
+    const [listaEqEntregados, setListaEqEntregados] = useState([]);
+    const [equipoSelecc, setEquipoSelecc] = useState({});
+
+    useEffect(() => {
+        if (isOpen && !abrirDetallesEqEntregado) {
+            ListarEquEntregados();
+        }
+    }, [isOpen]);
+
+    async function ListarEquEntregados() {
+        setListaEqEntregadosCompleta(await BuscarEquiposEntregados());
+    }
+
+    //Rellenar la lsita dinámica con la completa
+    useEffect(() => {
+        if (isOpen && !abrirDetallesEqEntregado) {
+            setListaEqEntregados(listaEqEntregadosCompleta);
+        }
+    }, [listaEqEntregadosCompleta]);
+
+    const ManejarCancelar = () => {
+        window.location.reload();
+    }
+
+    //Filtrar lista
+    const FiltrarLista = (criterio) => {
+        setListaEqEntregados(ListaFiltrada('cedulaCliente', listaEqEntregadosCompleta, criterio));
+    }
+
+    const SeleccEquipo = (equipo) => {
+        if(equipo) setEquipoSelecc(equipo);
+    }
+
+    //...Cuando se setee el equipo seleccionado
+    useEffect(() => {
+        if(isOpen){
+            VentanaDetalles();
+        }
+    }, [equipoSelecc]);
+
+    function VentanaDetalles(){
+        setAbrirDetallesEqEntregado(true);
+    }
 
     if (!isOpen) return null;
 
@@ -15,23 +61,36 @@ function ModalListaEqEntregados({ isOpen, onClose }) {
         <div className="modalTransparencia">
             <div className="modal">
                 <div id='formListaEqEntregados'>
-                    <input className='formListaCliChilds' id='inputFiltroEqEntregados'></input>
-                    <select name='filtrarListaCli' className='formListaCliChilds' id="selectFiltroEqEntregados">
-                        <option value="fecha">Fecha</option>
-                        <option value="cedula">Cédula</option>
-                        <option value="nombre">Nombre</option>
-                    </select>
+                    <input className='formListaCliChilds' id='inputFiltroEqEntregados'
+                        placeholder='Buscar por cédula solamente...'
+                        onChange={e => FiltrarLista(e.target.value)}></input>
                     <table className='formListaCliChilds' id="tablaListaEqEntregados">
-                        <tr className='editCliFilaTabla' onClick={() => setAbrirDetallesEqEntregado(true)}>
-                            <td >Fecha Uno</td>
-                            <td className='nombreListaEqEntregados'>Nombre del Cliente</td>
-                        </tr>
+                        <thead>
+                            <tr className='editCliFilaTabla tituloTabla'>
+                                <td >Fecha de Salida</td>
+                                <td className='nombreListaEqEntregados'>Cédula del Cliente</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                listaEqEntregados === null || listaEqEntregados === undefined ? <tr></tr> :
+                                    listaEqEntregados.map((equipo, index) => (
+                                        <tr onClick={() => SeleccEquipo(equipo)} key={index}>
+                                            <td>{equipo.fechaSalida.toString().substring(0, 10)}</td>
+                                            <td>{equipo.idCliente}</td>
+                                        </tr>
+                                    ))
+                            }
+                        </tbody>
                     </table>
                     <div id='contBotonesListaEqEntregados'>
-                        <BotonForm idImportadoDiv="btnCancelarListEqEntregados" textoBoton="Cancelar" classNameImportado="btnCancelar" onClickImportado={onClose} />
+                        <BotonForm idImportadoDiv="btnCancelarListEqEntregados"
+                            textoBoton="Cancelar" classNameImportado="btnCancelar"
+                            onClickImportado={ManejarCancelar} />
                     </div>
                 </div>
-                <VentanaDetallesEqEntregado isOpen={abrirDetallesEqEntregado} onClose={() => setAbrirDetallesEqEntregado(false)} />
+                <VentanaDetallesEqEntregado isOpen={abrirDetallesEqEntregado}
+                    onClose={() => setAbrirDetallesEqEntregado(false)} equipo={equipoSelecc}/>
             </div>
         </div>
     );
--- src/componentes/VentanaDetallesEqEntregado.js
+++ src/componentes/VentanaDetallesEqEntregado.js
@@ -1,43 +1,99 @@
 import './css/ventanaDetallesEqEntregado.css';
 import BotonForm from './BotonForm';
+import { useEffect, useState } from 'react';
+import { BuscarClientes } from '../apis/clienteApi';
+import { BuscarDetalles } from '../apis/detallesApi';
+import FormatearNumero from './CurrencyFormat';
 
-function VentanaDetallesEqEntregado({ isOpen, onClose }) {
+function VentanaDetallesEqEntregado({ isOpen, onClose, equipo }) {
+
+    const [equipoRecibido, setEquipoRecibido] = useState({});
+    const [listaClientes, setListaClientes] = useState([]);
+    const [cliente, setCliente] = useState({});
+    const [listaDetalles, setListaDetalles] = useState([]);
+
+    useEffect(() => {
+        if(isOpen){
+            setEquipoRecibido(equipo);
+            ListarClientes();
+            ListarDetalles();
+        }
+    }, [isOpen]);
+
+    const ListarClientes = async() => {
+        let listaAux = await BuscarClientes();
+        if(listaAux)setListaClientes(listaAux);
+        else console.log('Lista de clientes no encontrada');
+    }
+
+    async function ListarDetalles(){
+        let listaAux = await BuscarDetalles(equipoRecibido.id);
+        if(listaAux) setListaDetalles(listaAux);
+    }
+
+    //... lista Clientes set
+    useEffect(() => {
+        if(isOpen) EncontrarCliente();
+    }, [listaClientes]);
+
+    function EncontrarCliente(){
+        if(equipoRecibido && listaClientes){
+            let clienteAux = listaClientes.find(cliente => cliente['id'] === equipoRecibido.idCliente);
+            setCliente(clienteAux);
+        }else{
+            console.log('No se pudo encontrar el cleinte por equipo recibido null');
+        }
+    }
+
+    function Imprimir(){
+        alert('Le debo la impresión profe!');
+    }
 
     if (!isOpen) return null;
     return (
         <div id="formDetallesEqEntregado">
             <div id='tituloDetallesEqEntregado'>
-                <label>Nombre del Cliente</label>
-                <label>Fecha: dd/mm/aaaa</label>
+                <label>{ cliente ? cliente.nombre : ''}</label>
+                <label>Fecha: {equipo ? equipo.fechaIngreso.toString().substring(0, 10): ''}</label>
             </div>
             <div id='filaDosDetallesEqEntregado'>
-                <label>Ingreso por: xxxxxxxxx</label>
-                <label>Referencia o marca de equipo</label>
+                <label>Ingreso por: <u>{equipo ? equipo.tipoIngreso : ''}</u></label>
+                <label>{equipo ? equipo.modelo : 'Modelo'}</label>
             </div>
             <div id='filaTresDetallesEqEntregado'>
                 <label className='etDetalles'>Condiciones de Equipo Recibido:</label>
-                <textarea id='condEqRecibidoEqEntregado'></textarea>
+                <textarea id='condEqRecibidoEqEntregado' disabled
+                    value={equipo? equipo.condicionesFisicasRecibidas:'Error'}></textarea>
                 <label className='etDetalles'>Partes Internas del Equipo:</label>
-                <textarea id='partesEqRecibidoEqEntregado'></textarea>
+                <textarea id='partesEqRecibidoEqEntregado' disabled
+                    value={equipo? equipo.partesInternasRecibido:'Error'}></textarea>
                 <table>
+                    <tbody>
                     <tr>
                         <th>Descripción</th>
                         <th>Precio</th>
                     </tr>
+                    {
+                        !listaDetalles ? <tr></tr> : 
+                        listaDetalles.map((detalle, index) => (
+                            <tr key={index}>
+                                <td className='columDetalles'>{detalle.descripcion}</td>
+                                <td> $ {FormatearNumero(detalle.precio.toString())}</td>
+                            </tr>
+                        ))
+                    }
                     <tr>
-                        <td>Descripción Uno</td>
-                        <td>$ 000.000</td>
+                        <td className='celdaTotalTexto'><b>Total</b></td>
+                        <td><b>$ {FormatearNumero(equipo.precioTotal.toString())}</b></td>
                     </tr>
-                    <tr>
-                        <td>Total</td>
-                        <td>$ 000.000</td>
-                    </tr>
+                    </tbody>
                 </table>
-                <label className='etDetalles'>Días de Garantía Otorgados: XX</label>
+                <label className='etDetalles'>
+                    Días de Garantía Otorgados: <u>{equipo ? equipo.diasGarantia : ''}</u></label>
             </div>
             <div id='auxContBotonesDetallesEqEntregado'>
                 <div id='contBotonesDetallesEqEntregado' className='dosBotones'>
-                    <BotonForm textoBoton="Imprimir" classNameImportado="btnAceptar" />
+                    <BotonForm textoBoton="Imprimir" classNameImportado="btnAceptar" onClickImportado={Imprimir}/>
                     <BotonForm textoBoton="Atrás" classNameImportado="btnCancelar" onClickImportado={onClose} />
                 </div>
             </div>
